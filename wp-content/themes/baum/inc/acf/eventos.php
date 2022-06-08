8b926cd2114b5a21bfd883c150eb4c11c22b9eee
Kenny Poncio <kennyponcio07@gmail.com>
2022-06-08 00:43:34 -0600

    Creación de post-type Eventos y Taxonomía Artistas

---
+++ wp-content/themes/baum/inc/acf/eventos.php
@@ -0,0 +1,85 @@
+<?php
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_62a04070b2faf',
+	'title' => 'Eventos',
+	'fields' => array(
+		array(
+			'key' => 'field_62a0407b24cf6',
+			'label' => 'Ubicación',
+			'name' => 'ubicacion',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_62a0408724cf7',
+			'label' => 'Fecha',
+			'name' => 'fecha',
+			'type' => 'date_picker',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'display_format' => 'F j, Y',
+			'return_format' => 'F j, Y',
+			'first_day' => 1,
+		),
+		array(
+			'key' => 'field_62a040c424cf8',
+			'label' => 'Descripción',
+			'name' => 'descripcion',
+			'type' => 'textarea',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'maxlength' => '',
+			'rows' => '',
+			'new_lines' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'eventos',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+));
+
+endif;		
